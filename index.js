dfc53d6dd296fbcb7bd19f60a7868d451c70100c
PapiLuee <41023873+LueDev@users.noreply.github.com>
2023-09-21 06:16:59 -0400

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+console.log(currentUser);
+
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+console.log(welcomeMessage);
+
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " +currentUser.toUpperCase() + "!";
+console.log(excitedWelcomeMessage);
+
+let shortGreeting = "Welcome, " +currentUser.slice(0, 1) + "!";
+console.log(shortGreeting);
